302989be46509417beb0cbc9c862a66de631b9f7
Gabriel Sória Souza <maczepp@protonmail.com>
2020-06-22 22:12:09 -0300

    Initial Commit

---
+++ Shared/SuperPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperPlayerApp.swift
+//  Shared
+//
+//  Created by Gabriel Sória Souza on 22/06/20.
+//
+
+import SwiftUI
+
+@main
+struct SuperPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
